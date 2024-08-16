ae6665d14502b970ba1f6020206675894d504ec3
simonkadili47 <simonkadili47@gmail.com>
2024-08-16 14:47:01 +0100

    comit

--- src/Components/Destination/Destination.jsx
+++ src/Components/Destination/Destination.jsx
@@ -4,7 +4,6 @@ import image_2 from '../Destination/image2.png';
 import image_3 from '../Destination/image3.png';
 import image_4 from '../Destination/image4.png';
 import image_5 from '../Destination/image5.png'
-import image_6 from '../Destination/image6.png'
 import image_7 from '../Destination/image7.png'
 import image_8 from '../Destination/image8.png'
 
@@ -20,19 +19,19 @@ const Navlinks = [
 function Destination() {
   const renderCard = (image, location, place, days, price) => (
     <div className='flex flex-col items-start  border rounded-lg px-1 py-1 mt-4'>
-      <img src={image} alt="" className='w-auto   h-60 object-cover rounded-t-lg' />
-      <div className=' w-64  pb-2 mt-2'>
+      <img src={image} alt="" className='w-auto   h-36 object-cover rounded-t-lg' />
+      <div className=' w-40  pb-2 mt-2'>
         <div className='flex justify-between items-center w-full text-sm'>
           <div className='flex items-center'>
             <FaMapMarkerAlt className='mr-1 text-orange-500' />
             <p>{location}</p>
           </div>
-          <div className='flex items-center'>
+          <div className='flex items-center mr-1'>
             <FaStar className='text-yellow-500 mr-1' />
             <span>4.7</span>
           </div>
         </div>
-        <p className='mt-2 pl-2 text-start text-md font-bold'>{place}</p>
+        <p className='mt-3 pl-2 text-start text-md font-bold'>{place}</p>
         <p className='mt-2 pl-2 text-start text-sm'>{days}</p>
         <div className='flex items-center'>
           <p className='text-orange-500 font-bold text-2xl pl-2'>{price}</p>
@@ -74,7 +73,7 @@ function Destination() {
       {/* Repeat the cards with some space below */}
       <div className='grid grid-cols-1 md:grid-cols-4 gap-4 mt-2'>
         {renderCard(image_5, "Maldives", "Huruwalhi", "7 Days Tour on 2 person", "$620")}
-        {renderCard(image_6, "Indonesia", "Bali Province", "4 Days 2 person", "$780")}
+        {renderCard(image_2, "Indonesia", "Bali Province", "4 Days 2 person", "$780")}
         {renderCard(image_7, "Spain", "Barcelona City Beach", "4 Days Tour on 4 person", "$850")}
         {renderCard(image_8, "Maldives", "Huruwalhi", "7 Days Tour on 2 person", "$620")}
       </div>
